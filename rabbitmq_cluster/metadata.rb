b417da371643920a2b840cc033d299b34b2f1978
chrishenderson <chris.henderson@sage.com>
2014-10-07 08:00:34 +0100

    Separate rabbitmq_cluster in own repo

--- rabbitmq_cluster/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             "rabbitmq_cluster"
-
-%w{ubuntu debian linuxmint redhat centos scientific amazon fedora oracle smartos suse}.each do |os|
-  supports os
-end
-
-%w{ rabbitmq hostsfile }.each do |ckbk|
-  depends ckbk
-end
